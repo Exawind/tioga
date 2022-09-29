e0919e4bb77465e5b055c6469811f93f7a4e0f51
Jon Rood <jon.rood@nrel.gov>
2022-09-28 19:47:45 -0600

    Convert median.F90 to nth_element C++ algorithm.

---
+++ src/buildADTrecursion.h
@@ -0,0 +1,7 @@
+#ifndef BUILDADTRECURSION_H
+#define BUILDADTRECURSION_H
+void buildADTrecursion(double *coord, double *adtReals, double *adtWork,
+                       int *adtIntegers, int *elementsAvailable, int *adtCount,
+                       int side, int parent, int level, int ndim, int nelem,
+                       int nav);
+#endif
