df54c6d390344406b9e92ee399627370ac780f70
Jon Rood <jon.rood@nrel.gov>
2022-10-05 13:14:54 -0600

    Convert cellVolume.F90 to cellVolume.C.

--- src/math.c
+++ src/math.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "codetypes.h"
+#include "cellVolume.h"
 
 void solvec(double **a,double *b,int *iflag,int n)
 {
@@ -293,41 +294,44 @@ void computeNodalWeights(double xv[8][3],double *xp,double frac[8],int nvert)
     }
 }
 
-void cellvolume_(double*, double[][3], int[][6], int[][24], int*, int*);
-
 double computeCellVolume(double xv[8][3],int nvert)
 {
- double vol;
- int itype;
- int nfaces;
- int numverts[4][6]={3,3,3,3,0,0,4,3,3,3,3,0,3,4,4,4,3,0,4,4,4,4,4,4};
- int faceInfo[4][24]={1,2,3,0,1,4,2,0,2,4,3,0,1,3,4,0,0,0,0,0,0,0,0,0,
-                       1,2,3,4,1,5,2,0,2,5,3,0,4,3,5,0,1,4,5,0,0,0,0,0,
-                       1,2,3,0,1,4,5,2,2,5,6,3,1,3,6,4,4,6,5,0,0,0,0,0,
-                       1,2,3,4,1,5,6,2,2,6,7,3,3,7,8,4,1,4,8,5,5,8,7,6};
- switch(nvert)
-   {
-   case 4:
-     itype=0;
-     nfaces=4;
-     break;
-   case 5:
-     itype=1;
-     nfaces=5;
-     break;
-   case 6:
-     itype=2;
-     nfaces=5;
-     break;
-   case 8:
-     itype=3;
-     nfaces=6;
-     break;
-   }
-     
- cellvolume_(&vol,xv,&numverts[itype],&faceInfo[itype],&nfaces,&nvert);
- return vol;
+  double vol;
+  int itype = 0;
+  int nfaces = 0;
+  int numverts[4][6] = {3, 3, 3, 3, 0, 0,
+                        4, 3, 3, 3, 3, 0,
+                        3, 4, 4, 4, 3, 0,
+                        4, 4, 4, 4, 4, 4};
+  int faceInfo[4][6][4] = {
+      1, 2, 3, 0, 1, 4, 2, 0, 2, 4, 3, 0, 1, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      1, 2, 3, 4, 1, 5, 2, 0, 2, 5, 3, 0, 4, 3, 5, 0, 1, 4, 5, 0, 0, 0, 0, 0,
+      1, 2, 3, 0, 1, 4, 5, 2, 2, 5, 6, 3, 1, 3, 6, 4, 4, 6, 5, 0, 0, 0, 0, 0,
+      1, 2, 3, 4, 1, 5, 6, 2, 2, 6, 7, 3, 3, 7, 8, 4, 1, 4, 8, 5, 5, 8, 7, 6};
+
+  switch (nvert) {
+  case 4:
+    itype = 0;
+    nfaces = 4;
+    break;
+  case 5:
+    itype = 1;
+    nfaces = 5;
+    break;
+  case 6:
+    itype = 2;
+    nfaces = 5;
+    break;
+  case 8:
+    itype = 3;
+    nfaces = 6;
+    break;
+  }
+
+  cellVolume(vol, xv, numverts[itype], faceInfo[itype], nfaces, nvert);
+  return vol;
 }
+
 double tdot_product(double a[3],double b[3],double c[3])
 {
   int k;
