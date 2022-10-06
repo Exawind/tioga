d58c0a0b51dc6b43d9e306f7172e2747545aaf16
Jon Rood <jon.rood@nrel.gov>
2022-10-06 14:55:52 -0600

    Use 1d array for kaiser in C++ to match original array structure.

--- src/kaiser.C
+++ src/kaiser.C
@@ -1,8 +1,12 @@
 #include "kaiser.h"
 #include <cmath>
 
-void kaiser(double a[3][3], int nrows, int n, double eigenv[3], double trace,
-            double sume) {
+#define NROWS 3
+#define NCOLS 3
+
+// This functions expects a 3x3 matrix
+void kaiser_cpp(double a[9], int /*nrows*/, int /*n*/, double eigenv[3],
+                double trace, double sume) {
   const double small = 1.0e-12;
   const double zero = 0.0;
   const double half = 0.5;
@@ -10,39 +14,36 @@ void kaiser(double a[3][3], int nrows, int n, double eigenv[3], double trace,
   int i, iter, j, k, ncount, nn;
   double absp, absq, COS, ctn, eps, halfp, p, q, SIN, ss;
   double TAN, temp, xj, xk;
-  int ier;
 
-  ier = 1;
-  if ((n < 1) || (n > nrows)) {
-    return;
-  }
-  ier = 0;
+  // if ((n < 1) || (n > NROWS)) {
+  //   return;
+  // }
   iter = 0;
   trace = zero;
   ss = zero;
-  for (j = 1; j <= n; j++) {
-    trace = trace + a[j - 1][j - 1];
-    for (i = 1; i <= n; i++) {
-      ss = ss + a[j - 1][i - 1] * a[j - 1][i - 1];
+  for (j = 1; j <= NROWS; j++) {
+    trace = trace + a[(j - 1) * NROWS + (j - 1)];
+    for (i = 1; i <= NCOLS; i++) {
+      ss = ss + a[(j - 1) * NROWS + (i - 1)] * a[(j - 1) * NROWS + (i - 1)];
     }
   }
   sume = zero;
-  eps = small * ss / n;
-  nn = n * (n - 1) / 2;
+  eps = small * ss / NROWS;
+  nn = NROWS * (NROWS - 1) / 2;
   ncount = nn;
 
   //   ORTHOGONALIZE PAIRS OF COLUMNS J & K, K > J.
 twenty:
-  for (j = 1; j <= (n - 1); j++) {
-    for (k = j + 1; k <= n; k++) {
+  for (j = 1; j <= (NROWS - 1); j++) {
+    for (k = j + 1; k <= NROWS; k++) {
 
       //   CALCULATE PLANAR ROTATION REQUIRED
 
       halfp = zero;
       q = zero;
-      for (i = 1; i <= n; i++) {
-        xj = a[j - 1][i - 1];
-        xk = a[k - 1][i - 1];
+      for (i = 1; i <= NROWS; i++) {
+        xj = a[(j - 1) * NROWS + (i - 1)];
+        xk = a[(k - 1) * NROWS + (i - 1)];
         halfp = halfp + xj * xk;
         q = q + (xj + xk) * (xj - xk);
       }
@@ -85,10 +86,12 @@ twenty:
 
       //   PERFORM ROTATION
 
-      for (i = 1; i <= n; i++) {
-        temp = a[j - 1][i - 1];
-        a[j - 1][i - 1] = temp * COS + a[k - 1][i - 1] * SIN;
-        a[k - 1][i - 1] = -temp * SIN + a[k - 1][i - 1] * COS;
+      for (i = 1; i <= NROWS; i++) {
+        temp = a[(j - 1) * NROWS + (i - 1)];
+        a[(j - 1) * NROWS + (i - 1)] =
+            temp * COS + a[(k - 1) * NROWS + (i - 1)] * SIN;
+        a[(k - 1) * NROWS + (i - 1)] =
+            -temp * SIN + a[(k - 1) * NROWS + (i - 1)] * COS;
       }
     }
   }
@@ -97,14 +100,13 @@ twenty:
   if (iter < 10) {
     goto twenty;
   }
-  ier = 2;
 
   //   CONVERGED, OR GAVE UP AFTER 10 ITERATIONS
 onehundredsixty:
-  for (j = 1; j <= n; j++) {
+  for (j = 1; j <= NROWS; j++) {
     temp = 0.0;
-    for (int m = 1; m <= n; m++) {
-      temp += (a[j - 1][m - 1] * a[j - 1][m - 1]);
+    for (int m = 1; m <= NROWS; m++) {
+      temp += (a[(j - 1) * NROWS + (m - 1)] * a[(j - 1) * NROWS + (m - 1)]);
     }
     eigenv[j - 1] = std::sqrt(temp);
     sume = sume + eigenv[j - 1];
@@ -112,14 +114,14 @@ onehundredsixty:
 
   //   SCALE COLUMNS TO HAVE UNIT LENGTH
 
-  for (j = 1; j <= n; j++) {
+  for (j = 1; j <= NROWS; j++) {
     if (eigenv[j - 1] > zero) {
       temp = one / eigenv[j - 1];
     } else {
       temp = zero;
     }
-    for (int m = 1; m <= n; m++) {
-      a[j - 1][m - 1] = a[j - 1][m - 1] * temp;
+    for (int m = 1; m <= NROWS; m++) {
+      a[(j - 1) * NROWS + (m - 1)] = a[(j - 1) * NROWS + (m - 1)] * temp;
     }
   }
 
