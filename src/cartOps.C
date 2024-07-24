4d165b28e13334db8286701f58dfc62ff1894999
Jon Rood <jon.rood@nrel.gov>
2024-07-24 15:34:44 -0600

    Convert to snprintf.

--- src/cartOps.C
+++ src/cartOps.C
@@ -142,8 +142,8 @@ void MeshBlock::writeOBB2(OBB* obc, int bid)
     int l, k, j, m, il, ik, ij;
     REAL xx[3];
 
-    sprintf(intstring, "%d", 100000 + bid);
-    sprintf(fname, "cbox%s.dat", &(intstring[1]));
+    snprintf(intstring, sizeof(intstring), "%d", 100000 + bid);
+    snprintf(fname, sizeof(fname), "cbox%s.dat", &(intstring[1]));
     fp = fopen(fname, "w");
     fprintf(fp, "TITLE =\"Box file\"\n");
     fprintf(fp, "VARIABLES=\"X\",\"Y\",\"Z\"\n");
