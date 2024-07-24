a1167104bdf58259a70ba792db60a6a5c5ccf6fb
Jon Rood <jon.rood@nrel.gov>
2024-07-24 15:26:01 -0600

    Try snprintf.

--- src/CartBlock.C
+++ src/CartBlock.C
@@ -684,8 +684,8 @@ void CartBlock::writeCellFile(int bid)
     ibmax = -30000000;
     nnodes = (dims[1] + 1) * (dims[0] + 1) * (dims[2] + 1);
     ncells = dims[0] * dims[1] * dims[2];
-    sprintf(intstring, "%d", 100000 + myid);
-    sprintf(fname, "cart_cell%s.dat", &(intstring[1]));
+    snprintf(intstring, sizeof(intstring), "%d", 100000 + myid);
+    snprintf(fname, sizeof(fname), "cart_cell%s.dat", &(intstring[1]));
     if (bid == 0) {
         fp = fopen(fname, "w");
     } else {
